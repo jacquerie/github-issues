29d475e5b8e47fc63c75919f801aa6dcef601185
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2015-03-06 00:28:12 +0100

    Experiment with jQuery's style guide
     * Rewrite `issues.js` to conform to jQuery's style guide.

--- issues.js
+++ issues.js
@@ -1,83 +1,89 @@
-var github = require("octonode"),
-    parse = require("parse-link-header"),
-    util = require("util"),
-    _ = require("underscore");
+var github = require( "octonode" ),
+    parse = require( "parse-link-header" ),
+    util = require( "util" ),
+    _ = require( "underscore" ),
 
-var client = github.client(process.env.TOKEN);
+    client = github.client( process.env.TOKEN ),
 
-var countIssues = function (repo, issues) {
-  var partition = _.partition(issues, function (issue) {
-    return _.has(issue, "state") && (issue.state === "open");
-  });
+    countIssues = function( repo, issues ) {
+      var open,
+          closed,
+          partition = _.partition( issues, function( issue ) {
+        return _.has( issue, "state" ) && ( issue.state === "open" );
+      } );
 
-  var open = _.first(partition),
-    closed = _.last(partition);
+      open = _.first( partition ),
+      closed = _.last( partition );
 
-  console.log(util.format("REPO: %s\nTOTAL: %d\nOPEN: %d\nCLOSED: %d\n",
-                         repo, issues.length, open.length, closed.length));
-};
+      console.log( util.format( "REPO: %s\nTOTAL: %d\nOPEN: %d\nCLOSED: %d\n",
+                     repo, issues.length, open.length, closed.length ) );
+    },
 
-var allIssues = function (repo, page, issues, callback) {
-  var ghrepo = client.repo(repo);
+    allIssues = function( repo, page, issues, callback ) {
+      var ghrepo = client.repo( repo );
 
-  ghrepo.issues({
-    page: page,
-    per_page: 100,
-    state: "all",
-  }, function (err, data, headers) {
-    issues = issues.concat(data);
+      ghrepo.issues( {
+        page: page,
+        //jscs:disable requireCamelCaseOrUpperCaseIdentifiers
+        per_page: 100,
+        //jscs:enable requireCamelCaseOrUpperCaseIdentifiers
+        state: "all"
+      }, function( err, data, headers ) {
+        issues = issues.concat( data );
 
-    if (_.has(headers, "link")) {
-      var parsedLink = parse(headers.link);
+        if ( _.has( headers, "link" ) ) {
+          var parsedLink = parse( headers.link );
 
-      if (_.has(parsedLink, "next")) {
-        if (_.has(parsedLink.next, "page")) {
-          allIssues(repo, parsedLink.next.page, issues, callback);
+          if ( _.has( parsedLink, "next" ) ) {
+            if ( _.has( parsedLink.next, "page" ) ) {
+              allIssues( repo, parsedLink.next.page, issues, callback );
+            } else {
+              callback( repo, issues );
+            }
+          } else {
+            callback( repo, issues );
+          }
         } else {
-          callback(repo, issues);
+          callback( repo, issues );
         }
-      } else{
-        callback(repo, issues);
-      }
-    } else {
-      callback(repo, issues);
-    }
-  });
-};
+      } );
+    },
 
-var parseRepos = function (org, repos) {
-  _.each(repos, function (repo) {
-    if (_.has(repo, "name")) {
-      allIssues(org + "/" + repo.name, 0, [], countIssues);
-    }
-  });
-};
+    parseRepos = function( org, repos ) {
+      _.each( repos, function( repo ) {
+        if ( _.has( repo, "name" ) ) {
+          allIssues( org + "/" + repo.name, 0, [], countIssues );
+        }
+      } );
+    },
 
-var allRepos = function (org, page, repos, callback) {
-  var ghorg = client.org(org);
+    allRepos = function( org, page, repos, callback ) {
+      var ghorg = client.org( org );
 
-  ghorg.repos({
-    page: page,
-    per_page: 100,
-  }, function (err, data, headers) {
-    repos = repos.concat(data);
+      ghorg.repos( {
+        page: page,
+        //jscs:disable requireCamelCaseOrUpperCaseIdentifiers
+        per_page: 100
+        //jscs:enable requireCamelCaseOrUpperCaseIdentifiers
+      }, function( err, data, headers ) {
+        repos = repos.concat( data );
 
-    if (_.has(headers, "link")) {
-      var parsedLink = parse(headers.link);
+        if ( _.has( headers, "link" ) ) {
+          var parsedLink = parse( headers.link );
 
-      if (_.has(parsedLink, "next")) {
-        if (_.has(parsedLink.next, "page")) {
-          allRepos(org, parsedLink.next.page, repos, callback);
+          if ( _.has( parsedLink, "next" ) ) {
+            if ( _.has( parsedLink.next, "page" ) ) {
+              allRepos( org, parsedLink.next.page, repos, callback );
+            } else {
+              callback( org, repos );
+            }
+          } else {
+            callback( org, repos );
+          }
         } else {
-          callback(org, repos);
+          callback( org, repos );
         }
-      } else{
-        callback(org, repos);
-      }
-    } else {
-      callback(org, repos);
-    }
-  });
-};
+      } );
+    };
 
-allRepos("jquery", 0, [], parseRepos);
+allRepos( "jquery", 0, [], parseRepos );
